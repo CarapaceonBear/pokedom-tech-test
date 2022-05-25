52c6a5fe92137b57ca5d385be663865aec1c0378
carapaceonbear <seerhan17@gmail.com>
2022-05-25 14:38:51 +0100

    text search implemented

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 import {pokemonArray} from "./data/pokemon.js";
+const copyArray = [...pokemonArray];
 
 const cardContainer = document.getElementById("card-container");
-
-if (pokemonArray != null) {
-    console.log("Array imported");
-}
+const searchBox = document.getElementById("search-box");
+const findTypeOne = document.getElementById("find-type-one");
+const findTypeTwo = document.getElementById("find-type-two");
 
 const buildCard = (object) => {
     let pokemon = object;
@@ -12,25 +12,55 @@ const buildCard = (object) => {
     pokeName = `${pokeName[0].toUpperCase()}${pokeName.slice(1, pokeName.length + 1)}`
     return (
         `<div class="card">
-            <img class="card__image" src=${pokemon.sprite}>
-            <div class="card__content">
-                <h2 class="card__heading">
-                    ${pokeName}
-                </h2>
-                <p class="card__text">
-                    ${pokeName} (#${pokemon.id}) is a 
-                    ${
-                        pokemon.types.length == 1 ? pokemon.types[0] 
-                        : pokemon.types[0] + " & " + pokemon.types[1]
+        <img class="card__image" src=${pokemon.sprite}>
+        <div class="card__content">
+        <h2 class="card__heading">
+        ${pokeName}
+        </h2>
+        <p class="card__text">
+        ${pokeName} (#${pokemon.id}) is a 
+        ${
+            pokemon.types.length == 1 ? pokemon.types[0] 
+            : pokemon.types[0] + " & " + pokemon.types[1]
                     }
                     type pokemon.
-                </p>
-            </div>
-        </div>`
-    )
+                    </p>
+                    </div>
+                    </div>`
+                    )
 }
 
-pokemonArray.forEach((pokemon) => {
+const onSearchUpdated = (event) => {
+    let searchTerm = event.target.value;
+    if (searchTerm === "") {
+        copyArray.forEach((pokemon) => {
+            cardContainer.innerHTML += buildCard(pokemon);
+        });
+    }
+    let refinedArray = searchCards(searchTerm, copyArray)
+    cardContainer.innerHTML = "";
+    refinedArray.forEach((pokemon) => {
+        cardContainer.innerHTML += buildCard(pokemon);
+    });
+}
+
+const searchCards = (search, array) => {
+    let request = search.toLowerCase();
+    let filteredArray = array.filter((item) => {
+        return item.name.includes(request);
+    })
+    return filteredArray;
+}
+
+// on start
+if (copyArray != null) {
+    console.log("Array imported");
+}
+copyArray.forEach((pokemon) => {
     cardContainer.innerHTML += buildCard(pokemon);
 })
+
+searchBox.addEventListener("input", onSearchUpdated);
+// findTypeOne.addEventListener("change", onTypeOneSelected);
+// findTypeTwo.addEventListener("change", onTypeTwoSelected);
 
